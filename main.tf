412a828a061d11e46fac1171a4653b15963bd119
henry faleye <henry@hoitcs.com.au>
2022-10-27 09:23:26 +1000

    cn-commit 4

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-
-
-
-
 module "vpc" {
   source = "cloudposse/vpc/aws"
   # Cloud Posse recommends pinning every module to a specific version
@@ -49,10 +45,83 @@ module "private_subnets" {
   az_ngw_ids          = module.public_subnets.az_ngw_ids
 }
 
+
+resource "aws_security_group" "dev-vpc-sg" {
+  name = "dev-vpc-default-sg"
+  vpc_id = module.vpc.vpc_id
+  description = "Dev VPC Default Security Group"
+
+  ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all ip and ports outboun"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
+resource "aws_instance" "my-ec2-vm" {
+  ami = "ami-0be2609ba883822ec" # Amazon Linux
+  instance_type = "t2.micro"
+  subnet_id = module.private_subnets.az_subnet_ids["us-east-1a"]
+  key_name = "tf_cloud_kp"
+	#user_data = file("apache-install.sh")
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<h1>Welcome to  AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+    EOF  
+  vpc_security_group_ids = [ aws_security_group.dev-vpc-sg.id ]
+}
+
+
+resource "aws_instance" "my-ec2-vm2" {
+  ami = "ami-0be2609ba883822ec" # Amazon Linux
+  instance_type = "t2.micro"
+  subnet_id = module.private_subnets.az_subnet_ids["us-east-1b"]
+  key_name = "tf_cloud_kp"
+	#user_data = file("apache-install.sh")
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<h1>Welcome to AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+    EOF  
+  vpc_security_group_ids = [ aws_security_group.dev-vpc-sg.id ]
+}
+
+
 output "private_az_subnet_ids" {
   value = module.private_subnets.az_subnet_ids
 }
 
 output "public_az_subnet_ids" {
   value = module.public_subnets.az_subnet_ids
+}
+
+output "vpc_ids" {
+  value = module.vpc.vpc_id
 }
